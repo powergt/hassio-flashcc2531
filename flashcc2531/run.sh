a5d589ba884abb5a1a33410d018f33e3ef22d136
powergt <powergt@users.noreply.github.com>
2025-12-25 09:28:42 +0100

    Update firmware download and flashing process

--- flashcc2531/run.sh
+++ flashcc2531/run.sh
@@ -4,19 +4,12 @@ cd /flash_cc2531
 if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
 
 echo "Downloading firmware"
-if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_Home_1.2/bin/default/CC2531_DEFAULT_20201127.zip; then echo "firmware not found" && exit 1; fi
-
-echo "unziping"
-if ! 7z x CC2531_DEFAULT_20201127.zip; then echo "unzip failed" && exit 1; fi
-
-echo "backup firmware"
-./cc_read save.hex
-if ! cp save.hex /backup/save_$(date +%s).hex; then echo "could not backup firmware" && exit 1; fi
+if ! wget https://powergt.altervista.org/tmp/default.hex; then echo "firmware not found" && exit 1; fi
 
 echo "erase"
 ./cc_erase
 
 echo "flash firmware"
-./cc_write CC2531ZNP-Prod.hex
+./cc_write default.hex
 
 echo "Finished"
